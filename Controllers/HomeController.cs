ded887ecf04fd4677585989246d6674ea2117232
jontecodes <jonterufus@yahoo.com>
2019-07-16 15:26:02 -0500

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using surveymodel.Models;
+
+namespace surveymodel.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        //
+        [HttpPost("survey")]
+        public IActionResult Submission(Survey thisSurvey)
+        {
+            Survey someSurvey = new Survey()
+            {
+                Name = thisSurvey.Name,
+                Location = thisSurvey.Location
+            };
+            return View("survey", someSurvey);
+        }
+
+    }
+}
---
+++ Models/MyModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace surveymodel.Models
+{
+    public class Survey
+    {
+       public string Name {get;set;}
+       public string Location {get;set;}
+    //    public string Fav {get;set;}
+    //    public string Comment {get;set;}
+    }
+}
